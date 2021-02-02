8b376696376c8bcae6d9b3cee30018613171e37f
Stan de Groot <callmedunnie@gmail.com>
2021-02-02 16:17:38 +0100

    Spawner and blocker prototype made

---
+++ Assets/Scripts/Blocker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocker : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Zombie"))
+        {
+            print("collidet");
+            collision.gameObject.GetComponent<ZombieWalkTmp>().FlipVelocity();
+        }
+    }
+
+}
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    [Header("SpawnSettings")]
+    [SerializeField] private GameObject m_ZombiePrefab;
+    [SerializeField] private Transform m_SpawnPoint;
+    [SerializeField] private float m_SpawnRate;
+    [SerializeField] private int m_SpawnAmount;
+
+    public void StartWave()
+    {
+        StartCoroutine(SpawnZombie());
+    }
+
+    //Spawns a zombie as fast as the SpawnRate
+    public IEnumerator SpawnZombie()
+    {
+        for (int i = 0; i < m_SpawnAmount; i++)
+        {
+            yield return new WaitForSeconds(m_SpawnRate);
+            Instantiate(m_ZombiePrefab, m_SpawnPoint.position, Quaternion.identity);
+        }
+    }
+}
